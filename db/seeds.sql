6c139069ecab36c9c8463e7d0df45e73ef61c3ba
Quackenstien <donaldhawkins422@gmail.com>
2020-06-15 19:15:04 -0700

    built seeds file and schema file

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Chedder Burger');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Avacdo Burger');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Hamburger');
